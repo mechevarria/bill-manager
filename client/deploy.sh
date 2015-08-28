d67f9b459adb46dd53287ae02d3368a7022a7354
mechevarria <mike.echevarria@gmail.com>
2015-08-28 12:57:58 -0400

    update deployment scripts to include fstab creation of temporary log directories.  Prevents service errors

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,5 +1,7 @@
 export dist=~/git/bill-manager/client/dist
 
+sudo apt-get install apache2
 sudo cp 000-default.conf /etc/apache2/sites-available
 sudo ln -s ${dist} /var/www/html/client
+echo 'tmpfs      /var/log/apache2    tmpfs        defaults,noatime           0    0' | sudo tee --append /etc/fstab
 sudo service apache2 restart
