da619f039620e23d0a16c2685593dad440450043
mechevarria <mike.echevarria@gmail.com>
2016-06-19 09:56:07 -0400

    add grails domain test

---
+++ grails-api/src/test/groovy/api/BillSpec.groovy
@@ -0,0 +1,24 @@
+package api
+
+import grails.test.mixin.TestMixin
+import grails.test.mixin.Mock
+import grails.test.mixin.support.GrailsUnitTestMixin
+import spock.lang.*
+
+
+@TestMixin(GrailsUnitTestMixin)
+@Mock([Bill])
+class BillSpec extends Specification {
+
+    def setup() {
+        new Bill([Date: new Date(), month: '05', year: '2016']).save(flush: true)
+    }
+
+    def cleanup() {
+    }
+
+    void "bill is saved"() {
+        expect:"bill to be saved"
+          Bill.count() == 1
+    }
+}
