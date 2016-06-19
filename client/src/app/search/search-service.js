240ccb8d1ed50667dcff3e77fe35e97258088ef6
mechevarria <mike.echevarria@gmail.com>
2016-06-19 11:11:18 -0400

    add amount faceting ranges of $100

--- client/src/app/search/search-service.js
+++ client/src/app/search/search-service.js
@@ -52,6 +52,8 @@ app.factory('SearchSrvc', function($http, $q) {
 
         return deferred.promise;
     };
+  
+    factory.amountGap = 100;
 
     factory.get = function(params, operator) {
 
@@ -64,19 +66,24 @@ app.factory('SearchSrvc', function($http, $q) {
 
         var url = resource + '/select';
 
-        var config = {
+        //noinspection JSDuplicatedDeclaration
+      var config = {
             params: {
-                'wt': 'json',
-                'q': query,
-                'rows': 2147483647,
-                'TZ' : moment.tz.guess(),
-                'facet': true,
-                'facet.mincount': 1,
-                'facet.field': 'category',
-                'facet.range': 'item_date',
-                'facet.range.end': 'NOW/YEAR+1YEAR',
-                'facet.range.start': 'NOW/YEAR-100YEAR',
-                'facet.range.gap': '+1YEAR'
+              'wt': 'json',
+              'q': query,
+              'rows': 2147483647,
+              'TZ' : moment.tz.guess(),
+              'facet': true,
+              'facet.mincount': 1,
+              'facet.field': 'category',
+              'facet.range': ['item_date', 'price'],
+              'f.item_date.facet.range.end': 'NOW/YEAR+1YEAR',
+              'f.item_date.facet.range.start': 'NOW/YEAR-100YEAR',
+              'f.item_date.facet.range.gap': '+1YEAR',
+              'f.price.facet.range.start': '0',
+              'f.price.facet.range.end': '10000',
+              'f.price.facet.range.gap': factory.amountGap
+
             }
         };
 
