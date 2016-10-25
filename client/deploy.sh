fe101a9015f16c83f2da4037cd7d9a4ebc4c4cab
Mike Echevarria <mike.echevarria@gmail.com>
2016-10-25 12:11:01 -0400

    add proxy mode to apache2 install process

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,6 +1,7 @@
 export dist=~/git/bill-manager/client/dist
 
 sudo apt-get install apache2
+sudo a2enmod proxy
 sudo cp 000-default.conf /etc/apache2/sites-available
 sudo ln -s ${dist} /var/www/html/client
 sudo service apache2 restart
