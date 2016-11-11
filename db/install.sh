533d168c6c6307e5b7fb9bf9d11e56ddaaf0f9ea
Mike Echevarria <mike.echevarria@gmail.com>
2016-11-11 15:10:24 -0500

    add sql workbench

--- db/install.sh
+++ db/install.sh
@@ -1,4 +1,3 @@
 sudo apt-get install mysql-server mysql-workbench
 mysql -u root -p < config.sql
-echo 'tmpfs      /var/log/mysql    tmpfs        defaults,noatime           0    0' | sudo tee --append /etc/fstab
 sudo update-rc.d mysql defaults
