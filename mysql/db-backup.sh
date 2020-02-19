4acee44cf4b1ef8abda86065c9a985dd4e8c4431
mechevarria <mike.echevarria@gmail.com>
2020-02-19 15:15:09 -0500

    change command to work on docker instance

--- mysql/db-backup.sh
+++ mysql/db-backup.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-mysqldump -u root -p billDb | gzip -v > ../../billDb.sql.gz
-
-# Backup
-# docker exec mysql /usr/bin/mysqldump -u root --password=root billDb | gzip -v > ../billDb.sql.gz
+docker exec mysql /usr/bin/mysqldump -u root --password=root billDb | gzip -v > ../billDb.sql.gz
--- mysql/db-restore.sh
+++ mysql/db-restore.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-gunzip < ../../*.sql.gz | mysql -u root -p billDb
-
-# Restore
-# gunzip < ../*.sql.gz | docker exec -i mysql /usr/bin/mysql -u root --password=root billDb
+gunzip < ../*.sql.gz | docker exec -i mysql /usr/bin/mysql -u root --password=root billDb
