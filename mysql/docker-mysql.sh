5dfcf4ef592b853e98f2c0d76440b461b11ee0eb
Mike Echevarria <mike.echevarria@gmail.com>
2020-11-05 21:36:32 -0500

    update to relative path and add data to ignore

--- mysql/docker-mysql.sh
+++ mysql/docker-mysql.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # create directory for mysql data
-mkdir -p /home/vmuser/mysql-data
+mkdir -p ../mysql-data
 
 # create network for other containers to communicate via name
 docker network create app-net
@@ -11,7 +11,7 @@ docker rm mysql
 docker run \
     --name=mysql \
     --network app-net \
-    --mount type=bind,src=/home/vmuser/mysql-data,dst=/var/lib/mysql \
+    --mount type=bind,src=${HOME}/git/bill-manager/mysql-data,dst=/var/lib/mysql \
     -e MYSQL_ROOT_PASSWORD=root \
     -e MYSQL_DATABASE=billDb \
     -e MYSQL_USER=app \
