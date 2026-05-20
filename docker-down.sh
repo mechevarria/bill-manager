296066f62fb506a89284e7ee2ef3bb5044c272c1
Mike Echevarria <mechevarria@seekr.com>
2026-05-20 10:44:53 -0400

    feat: docker compose for the entire simplified system

--- docker-down.sh
+++ docker-down.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker stop nginx springboot mysql
+docker compose down
--- docker-up.sh
+++ docker-up.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env bash
 
-cd /volume1/docker/bill-manager
-
-cd mysql
-./docker-mysql.sh
-
-sleep 3
-
-cd ../springboot-api
-./docker-springboot.sh
-
-cd ../client
-./docker-nginx.sh
+docker compose up -d --build
