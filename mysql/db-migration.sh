ee7b546ba66858cb1422dd2e0b78715daaacf9f8
mechevarria <mike.echevarria@gmail.com>
2026-05-20 16:35:05 -0400

    fix: db migration script for new version

---
+++ mysql/db-migration.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Adds AUTO_INCREMENT to all entity id columns.
+# Required when restoring a database created before the switch from
+# GenerationType.AUTO to GenerationType.IDENTITY in Hibernate.
+docker exec mysql /usr/bin/mysql -u root --password=root billDb -e "
+SET FOREIGN_KEY_CHECKS=0;
+ALTER TABLE bill            MODIFY id bigint NOT NULL AUTO_INCREMENT;
+ALTER TABLE expense         MODIFY id bigint NOT NULL AUTO_INCREMENT;
+ALTER TABLE income          MODIFY id bigint NOT NULL AUTO_INCREMENT;
+ALTER TABLE detail          MODIFY id bigint NOT NULL AUTO_INCREMENT;
+ALTER TABLE default_expense MODIFY id bigint NOT NULL AUTO_INCREMENT;
+ALTER TABLE default_income  MODIFY id bigint NOT NULL AUTO_INCREMENT;
+ALTER TABLE defaults        MODIFY id bigint NOT NULL AUTO_INCREMENT;
+ALTER TABLE owner           MODIFY id bigint NOT NULL AUTO_INCREMENT;
+SET FOREIGN_KEY_CHECKS=1;
+"
