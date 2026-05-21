c89243eadbdf2289eeba12b2be552c0de292067e
mechevarria <mike.echevarria@gmail.com>
2026-05-21 15:22:05 -0400

    fix: hibernate migration fix

--- mysql/db-migration.sh
+++ mysql/db-migration.sh
@@ -14,4 +14,13 @@ ALTER TABLE default_income  MODIFY id bigint NOT NULL AUTO_INCREMENT;
 ALTER TABLE defaults        MODIFY id bigint NOT NULL AUTO_INCREMENT;
 ALTER TABLE owner           MODIFY id bigint NOT NULL AUTO_INCREMENT;
 SET FOREIGN_KEY_CHECKS=1;
+-- Reset counters to max(id)+1 in case data was restored with explicit IDs
+ALTER TABLE bill            AUTO_INCREMENT = 1;
+ALTER TABLE expense         AUTO_INCREMENT = 1;
+ALTER TABLE income          AUTO_INCREMENT = 1;
+ALTER TABLE detail          AUTO_INCREMENT = 1;
+ALTER TABLE default_expense AUTO_INCREMENT = 1;
+ALTER TABLE default_income  AUTO_INCREMENT = 1;
+ALTER TABLE defaults        AUTO_INCREMENT = 1;
+ALTER TABLE owner           AUTO_INCREMENT = 1;
 "
